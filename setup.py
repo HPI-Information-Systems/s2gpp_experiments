87b12a6c6936fe467fd302330fcc9b5c22522a8d
wenig <phillip.wenig@hpi.de>
2022-06-10 18:03:31 +0200

    fix: setup description

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name="timeeval_hyperopt",
+    name="s2gpp_experiments",
     version='0.1.0',
     packages=find_packages(),
     license='MIT',
     author='Phillip Wenig',
     author_email='phillip.wenig@hpi.de',
-    description='Hyperopt for TimeEval Algorithms',
+    description='Experiments for S2G++',
     install_requires=required,
     python_requires=">=3.8",
     zip_safe=False

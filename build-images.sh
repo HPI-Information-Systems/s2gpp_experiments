ba3035fa58b27fe05f57d80d8ba5ba9c72670dc6
wenig <phillip.wenig@hpi.de>
2022-07-01 16:11:23 +0200

    feat: add generation of base images

--- build-images.sh
+++ build-images.sh
@@ -3,6 +3,12 @@
 git clone https://github.com/HPI-Information-Systems/TimeEval-algorithms.git
 git clone https://github.com/HPI-Information-Systems/S2Gpp.git
 
+baseImages=(
+  python3-base
+  r4-base
+  python3-torch
+)
+
 algorithms=(
     mstamp
     dbstream
@@ -12,6 +18,10 @@ algorithms=(
     torsk
     damp
 )
+
+for base in "${baseImages[@]}"; do
+  docker build -t registry.gitlab.hpi.de/akita/i/$base ./TimeEval-algorithms/0-base-images/$base
+done
 
 for algorithm in "${algorithms[@]}"; do
   docker build -t registry.gitlab.hpi.de/akita/i/$algorithm ./TimeEval-algorithms/$algorithm
